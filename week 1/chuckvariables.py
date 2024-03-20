2d812d812b3e269ee7382c27379c47c1d7aaad16
kepher99 <otienokepher9@gmail.com>
2024-03-20 23:36:52 +0300

    chuckNetworks

---
+++ week 1/chuckvariables.py
@@ -0,0 +1,18 @@
+#Lets build a robot Barista!!
+
+print("Hello, welcome KK coffee shop!!!!")
+
+name = input("What is your name?\n")
+
+print("Hello " + name + ", THANK YOU so much for coming in today.")
+
+menu = "Black coffee, Espresso, Latte, Cappuccino\n"
+
+#print(name + ", what would you like from our menu today? Here is what we are serving.\n\n"
+#     + menu)
+
+order = input(name + ", what would you like from our menu today? Here is what we are serving.\n\n"
+     + menu)
+
+print("Sounds good " + name + " , we'll have that " + order + " ready for you in a moment.")
+
